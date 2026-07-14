b0c6e44a60a088ed3276c03723e20aef3438cf71
Maksim <zharkov_maksim@mail.ru>
2026-07-14 15:31:06 +0300

    feat: add Page Object and parameterization for FAQ tests

--- src/test/java/tests/FaqTest.java
+++ src/test/java/tests/FaqTest.java
@@ -1,21 +1,48 @@
 package tests;
 
+import pages.HomePageFaq;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class FaqTest {
 
     private WebDriver driver;
+    private final int questionIndex;
+    private final String expectedAnswer;
+
     private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
+
+    @Parameterized.Parameters(name = "Вопрос {0}")
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {0, "Сутки — 400 рублей. Оплата курьеру наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет. Но если что — перезвоните."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на 8 суток — даже если будете кататься без передышки и без подзарядки. А вот зарядку не привозим."},
+                {6, "Да, если самокат ещё не привезли. Отменить можно в статусе «В поиске»."},
+                {7, "Да, привозим. Только в пределах МКАД."}
+        });
+    }
+
+    public FaqTest(int questionIndex, String expectedAnswer) {
+        this.questionIndex = questionIndex;
+        this.expectedAnswer = expectedAnswer;
+    }
 
     @Before
     public void setUp() {
@@ -25,42 +52,20 @@ public class FaqTest {
     }
 
     @Test
-    public void testFaqAnswersAreDisplayed() {
-        for (int i = 0; i < 8; i++) {
-            By question = By.id("accordion__heading-" + i);
-            By answer = By.xpath("//div[@id='accordion__panel-" + i + "']/p");
-            driver.findElement(question).click();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(answer));
-            String answerText = driver.findElement(answer).getText();
-            assertTrue("Ответ на вопрос " + i + " пустой", answerText.length() > 0);
-        }
+    public void testFaqAnswerIsDisplayed() {
+        HomePageFaq homePageFaq = new HomePageFaq(driver);
+        homePageFaq.clickQuestion(questionIndex);
+        assertTrue("Ответ на вопрос " + questionIndex + " не отображается",
+                homePageFaq.isAnswerDisplayed(questionIndex));
     }
 
     @Test
     public void testFaqAnswerTextIsCorrect() {
-        Object[][] faqData = {
-                {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру наличными или картой."},
-                {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
-                {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
-                {"Можно ли заказать самокат прямо сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
-                {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет. Но если что — перезвоните."},
-                {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на 8 суток — даже если будете кататься без передышки и без подзарядки. А вот зарядку не привозим."},
-                {"Можно ли отменить заказ?", "Да, если самокат ещё не привезли. Отменить можно в статусе «В поиске»."},
-                {"Я живу за МКАДом, привезёте?", "Да, привозим. Только в пределах МКАД."}
-        };
-
-        for (Object[] data : faqData) {
-            String questionText = (String) data[0];
-            String expectedAnswer = (String) data[1];
-            By questionLocator = By.xpath("//div[contains(@class, 'accordion__button') and text()='" + questionText + "']");
-            driver.findElement(questionLocator).click();
-            By answerLocator = By.xpath("//div[contains(@class, 'accordion__panel')]//p");
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(answerLocator));
-            String actualAnswer = driver.findElement(answerLocator).getText();
-            assertEquals("Ответ на вопрос: " + questionText, expectedAnswer, actualAnswer);
-        }
+        HomePageFaq homePageFaq = new HomePageFaq(driver);
+        homePageFaq.clickQuestion(questionIndex);
+        String actualAnswer = homePageFaq.getAnswerText(questionIndex);
+        assertEquals("Ответ на вопрос " + questionIndex + " не совпадает",
+                expectedAnswer, actualAnswer);
     }
 
     @After
